5aad0a6412157ece0315d1ef05e9673de918274f
Caleb James <calebsjames@gmail.com>
2021-05-24 14:59:01 -0500

    build:  What are the respective total sales for each of those years?

--- .vscode/total_sales_{year}.sql
+++ .vscode/total_sales_{year}.sql
@@ -2,3 +2,4 @@ SELECT SUM (total)
 FROM Invoice
 WHERE InvoiceDate BETWEEN "2009-01-01" AND "2009-12-31"
     OR InvoiceDate BETWEEN "2011-01-01" AND "2011-12-31"
+GROUP BY strftime ("%Y", InvoiceDate)
