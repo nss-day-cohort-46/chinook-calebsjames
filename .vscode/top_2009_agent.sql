64b04a42a82793bd7836e5b671a8f0c22e48eab1
Caleb James <calebsjames@gmail.com>
2021-05-25 10:23:30 -0500

    Which sales agent made the most in sales in 2009?

---
+++ .vscode/top_2009_agent.sql
@@ -0,0 +1,10 @@
+SELECT Employee.FirstName as FirstName, Employee.lastName as LastName, SUM(total)
+FROM Customer
+    INNER JOIN Employee
+    on Employee.EmployeeId = Customer.SupportRepId
+    INNER JOIN Invoice
+    on Invoice.CustomerId = Customer.CustomerId
+WHERE InvoiceDate BETWEEN "2009-01-01" AND "2009-12-31"
+
+GROUP BY EmployeeId
+ORDER BY Total ASC
