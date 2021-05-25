38a0a6dd9e6fbe2e33113e1c68065e2ba8d012f0
Caleb James <calebsjames@gmail.com>
2021-05-25 10:28:22 -0500

    build: Which sales agent made the most in sales over all?

--- .vscode/top_agent.sql
+++ .vscode/top_agent.sql
@@ -5,11 +5,7 @@ SELECT MAX(Total), FirstName, lastName
             on Employee.EmployeeId = Customer.SupportRepId
             INNER JOIN Invoice
             on Invoice.CustomerId = Customer.CustomerId
-        WHERE InvoiceDate BETWEEN "2009-01-01" AND "2009-12-31"
-    
-
-
-
+        
 
 GROUP BY EmployeeId
 ORDER BY Total DESC) 
